90df47205529db6faf56b05a3b1c5d96f561c37c
Joe Leung <joeleung32@yahoo.com.hk>
2021-05-19 08:19:46 +0800

    scan markdown script update, pass arg from cmd

--- utils/markdown/run.js
+++ utils/markdown/run.js
@@ -1,12 +1,17 @@
+const process = require('process');
 const {FS} = require('./core')
 
 try {
-    const MD = new FS('./src/mds')
-    let list
-    list = MD.setupMetaData(MD.scan())
-    MD.saveToJson('./src/mds/_meta/documents.json', list.documents)
-    MD.saveToJson('./src/mds/_meta/categories.json', list.categories)
-    MD.saveToJson('./src/mds/_meta/tags.json', list.tags)
+    const ArgDirname = process.argv.find(arg => arg.indexOf('--dirname') === 0);
+    if (ArgDirname && ArgDirname.split('=').length) {
+        const dirname = ArgDirname.split('=')[1];
+        const MD = new FS(dirname)
+        let list
+        list = MD.setupMetaData(MD.scan())
+        MD.saveToJson(`${dirname}/_meta/documents.json`, list.documents)
+        MD.saveToJson('${dirname}/_meta/categories.json', list.categories)
+        MD.saveToJson('${dirname}/_meta/tags.json', list.tags)
+    }
 } catch (e) {
     console.error(e)
 }
