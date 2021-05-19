af477ccae0bb674f32e15e3c1e2b955eaba349dd
Joe Leung <joeleung32@yahoo.com.hk>
2021-05-19 08:15:38 +0800

    deploy script update, pass arg from cmd

--- utils/deploy/run.js
+++ utils/deploy/run.js
@@ -1,11 +1,15 @@
+const process = require('process');
 const {FTP, FS} = require('./core')
 
 try {
-    const dirname = './dist/govhkdata'
-    const DIST = new FS()
-    const JOB = new FTP()
-    DIST.setDateMarker(dirname)
-    JOB.autoDeploy(DIST.scan(dirname))
+    const ArgDirname = process.argv.find(arg => arg.indexOf('--dirname') === 0);
+    if (ArgDirname && ArgDirname.split('=').length) {
+        const dirname = ArgDirname.split('=')[1];
+        const DIST = new FS()
+        const JOB = new FTP()
+        DIST.setDateMarker(dirname)
+        JOB.autoDeploy(DIST.scan(dirname))
+    }
 } catch (e) {
     console.error(e)
 }
